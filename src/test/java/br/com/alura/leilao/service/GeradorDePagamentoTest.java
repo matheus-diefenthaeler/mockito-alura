258448baf6dcac8524589ffc2994a13798198062
Matheus Diefenthaeler <matheus.axo@gmail.com>
2021-08-20 14:34:37 -0300

    GeradorDePagamentosTests

---
+++ src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
@@ -0,0 +1,73 @@
+package br.com.alura.leilao.service;
+
+import br.com.alura.leilao.dao.PagamentoDao;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Pagamento;
+import br.com.alura.leilao.model.Usuario;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import java.math.BigDecimal;
+import java.time.*;
+
+class GeradorDePagamentoTest {
+
+    private GeradorDePagamento gerador;
+
+    @Mock
+    private PagamentoDao pagamentoDao;
+
+    @Captor
+    private ArgumentCaptor<Pagamento> captor;
+
+    @Mock
+    private Clock clock;
+
+    @BeforeEach
+    public void beforeEach() {
+        MockitoAnnotations.initMocks(this); //Ler as anotações do Mockito e inicializar-las
+        this.gerador = new GeradorDePagamento(pagamentoDao, clock);
+    }
+
+    @Test
+    public void deveriaCriarPagamentoParaVencedorDoLeialao() {
+        Leilao leilao = leilao();
+        Lance vencedor = leilao.getLanceVencedor();
+
+
+        LocalDate data = LocalDate.of(2021,8,20);
+
+        Instant instant = data.atStartOfDay(ZoneId.systemDefault()).toInstant();
+
+        Mockito.when(clock.instant()).thenReturn(instant);
+        Mockito.when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+
+        gerador.gerarPagamento(vencedor);
+
+        Mockito.verify(pagamentoDao).salvar(captor.capture());  //Captor, capturar determinado objeto que foi criado e fazer as assertivas
+
+        Pagamento pagamento = captor.getValue();
+
+        Assertions.assertEquals(LocalDate.now().plusDays(3), pagamento.getVencimento());
+        Assertions.assertEquals(vencedor.getValor(),pagamento.getValor());
+        Assertions.assertFalse(pagamento.getPago());
+        Assertions.assertEquals(vencedor.getUsuario(),pagamento.getUsuario());
+        Assertions.assertEquals(leilao,pagamento.getLeilao());
+
+    }
+
+    private Leilao leilao() {
+
+        Leilao leilao = new Leilao("Celular", new BigDecimal("500"), new Usuario("Fulano"));
+        Lance lance = new Lance(new Usuario("Ciclano"), new BigDecimal("900"));
+
+        leilao.propoe(lance);
+        leilao.setLanceVencedor(lance);
+
+        return leilao;
+    }
+
+}
